73897625b9eb7db313988ff1f867a4d4e6c3b76a
Chikseen <Tim_Menzel@t-online.de>
2021-08-16 17:32:33 +0200

    Init Darkmode responiv fix

---
+++ js/colorswitch.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".color-mode");
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+  } 
+  else {
+    document.body.classList.toggle("dark-theme");
+  }
+});
