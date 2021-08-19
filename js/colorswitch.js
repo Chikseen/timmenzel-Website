eaaa2b7a64279c4a3fcb4271c6227b7dca9b44d6
Chikseen <Tim_Menzel@t-online.de>
2021-08-19 14:28:50 +0200

    Add function to color-mode-button

--- js/colorswitch.js
+++ js/colorswitch.js
@@ -2,9 +2,26 @@
 document.querySelector(".color-mode").addEventListener("click", function () {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.body.classList.toggle("light-theme");
-  } 
+    switchbutton();
+    } 
   else {
     document.body.classList.toggle("dark-theme");
+    switchbutton();
   }
 });
+
+var isLight
+
+function switchbutton(){
+  if (isLight) {
+    document.querySelector(".switch").classList.add("myclass1");
+    document.querySelector(".switch").style.transform = 'translateX(0%)';
+    isLight = false;
+  }
+  else {
+    document.querySelector(".switch").classList.add("myclass1");
+    document.querySelector(".switch").style.transform = 'translateX(100%)';
+    isLight = true;
+  }
+}
  
